51605b426124259e8b5b16f6b8d46852ac864fc5
d4n1 <d4n1.551@gmail.com>
2022-01-08 18:50:00 +0500

    feat(flash): support flashing outside recovery

--- push-bootimg.sh
+++ push-bootimg.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-echo "Pushing kexec_boot.img to device (/tmp)"
-adb shell "rm /tmp/kexec_boot.img" || true
-adb push out/kexec_boot.img /tmp || exit 1
+INREC="$(adb devices | grep recovery)"
+
+echo "Pushing kexec_boot.img to device (/tmp or /data/tmp/tmp)"
+
+PUSH_PATH="/tmp/kexec_boot.img"
+
+if [ -z "${INREC}" ]; then
+    ## Device is not in recovery (assumed in android), so create required stuff for push
+    echo "WARN: Device is not in recovery mode, this will pollute /sdcard"
+    PUSH_PATH="/sdcard/kexec_boot.img"
+fi
+
+adb shell "rm $PUSH_PATH" || true
+adb push out/kexec_boot.img "$PUSH_PATH" || exit 1
 
 echo "Would you like to flash the boot img?"
 select yn in "Yes" "No"; do
@@ -13,7 +24,7 @@ select yn in "Yes" "No"; do
 done
 
 echo "Flashing kexec_boot.img to device"
-adb shell 'su -c "dd if=/tmp/kexec_boot.img of=/dev/block/by-name/boot"' || adb shell 'sh -c "dd if=/tmp/kexec_boot.img of=$(ls /dev/block/platform/*/by-name/boot)"' || exit  1
+adb shell "su -c 'dd if=$PUSH_PATH of=/dev/block/by-name/boot'" || adb shell "sh -c 'dd if=$PUSH_PATH of=\$(ls /dev/block/platform/*/by-name/boot)'" || exit  1
 
 echo "Flashing done. Would you like to reboot?"
 select yn in "Yes" "No"; do
